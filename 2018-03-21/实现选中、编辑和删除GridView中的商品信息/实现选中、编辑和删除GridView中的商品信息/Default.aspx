16155034d830de0d709a9df08668bf12d64d0684
Clark-Chen-Dev <30585682+Clark-Chen-Dev@users.noreply.github.com>
2018-04-27 19:32:53 +0800

    实现选中、编辑和删除GridView中的商品信息

---
+++ 2018-03-21/实现选中、编辑和删除GridView中的商品信息/实现选中、编辑和删除GridView中的商品信息/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="实现选中_编辑和删除GridView中的商品信息.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>实现选中、编辑和删除GridView中的商品信息</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+                BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px"
+                CellPadding="4" GridLines="Horizontal"
+                OnRowCancelingEdit="GridView1_RowCancelingEdit"
+                OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing"
+                OnRowUpdating="GridView1_RowUpdating" PageSize="4"
+                Style="font-size: small" AllowPaging="True"
+                OnPageIndexChanging="GridView1_PageIndexChanging">
+                <RowStyle BackColor="White" ForeColor="#333333" />
+
+                <Columns>
+                    <asp:BoundField DataField="BccdID" HeaderText="编号" ReadOnly="True" />
+                    <asp:BoundField DataField="BccdName" HeaderText="版本名称" />
+                    <asp:BoundField DataField="BccdPrice" HeaderText="词典价格" />
+                    <asp:BoundField DataField="BccdSaleDate" DataFormatString="{0:d}"
+                        HeaderText="发行日期" />
+                    <asp:BoundField DataField="BccdInStock" HeaderText="现有库存量"/>
+                    <asp:CommandField HeaderText="选择" ShowSelectButton="True" />
+                    <asp:CommandField ButtonType="Image" CancelImageUrl="~/Images/BtnCancel.gif"
+                        EditImageUrl="~/Images/BtnUpdate.gif" HeaderText="编辑" ShowEditButton="True"
+                        UpdateImageUrl="~/Images/BtnSave.gif" />
+                    <asp:TemplateField HeaderText="删除" ShowHeader="False">
+                        <ItemTemplate>
+                            <asp:ImageButton ID="ImageButton1" runat="server" CommandName="Delete" 
+                                ImageUrl="~/Images/BtnDelete.gif"
+                                OnClientClick="return confirm('确定删除吗？');" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+
+                <FooterStyle BackColor="White" ForeColor="#333333" />
+                <PagerStyle BackColor="#336666" ForeColor="White" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="#339966" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#336666" Font-Bold="True" ForeColor="White" />
+            </asp:GridView>
+
+            <i>
+                <asp:Label ID="lblMessage" runat="server"></asp:Label>
+
+                您当前正在查看的页码：<b><font color="#ff0000"><%=GridView1.PageIndex + 1%>
+                        /
+                        <%=GridView1.PageCount%>
+                    </font></b></i>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2018-03-21/实现选中、编辑和删除GridView中的商品信息/实现选中、编辑和删除GridView中的商品信息/Default.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 实现选中_编辑和删除GridView中的商品信息
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();//调用自定义方法绑定数据到控件
+            }
+        }
+
+        public void BindData()
+        {
+            GridView1.DataSource = DBHelper.GetData("select * from mrbccd");//设置GridView控件的数据源为创建的数据集ds
+
+            //将数据库表中的主键字段放入GridView控件的DataKeyNames属性中
+            GridView1.DataKeyNames = new string[] { "BccdID" };
+            GridView1.DataBind();//绑定数据库表中数据
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //定义一个整型变量count存储根据当前行索引获取到的指定单元格中的字段值
+            int count = int.Parse(GridView1.Rows[e.RowIndex].Cells[4].Text.Trim());
+            if (count > 0)
+            {
+                e.Cancel = true; // 取消删除动作
+                lblMessage.Text = "警告：库存量大于0的商品不得删除！";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+
+            string delete_sql = "delete from mrbccd where BccdID='" + GridView1.DataKeys[e.RowIndex].Value.ToString() + "'";
+            int rowAffected = DBHelper.ExecuteNonQuery(delete_sql);
+            if (rowAffected > 0)
+            {
+                Response.Write("<script>alert('删除成功！')</script>");
+                BindData();//调用自定义方法重新绑定控件中数据
+            }
+            else
+            {
+                Response.Write("<script>alert('删除失败！')</script>");
+            }
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            //取得编辑行的关键字段的值
+            string bccdID = GridView1.DataKeys[e.RowIndex].Value.ToString();
+
+            //取得文本框中输入的内容
+            string bccdName = ((TextBox)(GridView1.Rows[e.RowIndex].Cells[1].Controls[0])).Text.ToString().Trim();
+            string bccdPrice = ((TextBox)(GridView1.Rows[e.RowIndex].Cells[2].Controls[0])).Text.ToString().Trim();
+            string bccdSaleDate = ((TextBox)(GridView1.Rows[e.RowIndex].Cells[3].Controls[0])).Text.ToString().Trim();
+            string bccdInStock = ((TextBox)(GridView1.Rows[e.RowIndex].Cells[4].Controls[0])).Text.ToString().Trim();
+
+            //定义更新操作的SQL语句
+            string update_sql = "update mrbccd set BccdName='" + bccdName + "',BccdPrice='" + bccdPrice + 
+                "',bccdSaleDate='" + Convert.ToDateTime(bccdSaleDate) + 
+                "',BccdInStock='" + bccdInStock + "' where BccdID='" + bccdID + "'";
+
+            int rowAffected = DBHelper.ExecuteNonQuery(update_sql);
+            if (rowAffected > 0)
+            {
+                Response.Write("<script>alert('修改成功！')</script>");
+                //设置GridView控件的编辑项的索引为－1，即取消编辑
+                GridView1.EditIndex = -1;
+                BindData();
+            } // end if
+            else
+            {
+                Response.Write("<script>alert('修改失败！');</script>");
+            } // end else
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            //设置GridView控件的编辑项的索引为－1，即取消编辑
+            GridView1.EditIndex = -1;
+            BindData();//数据绑定
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindData();//数据绑定
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            BindData();//数据绑定
+        }
+    }
+}
