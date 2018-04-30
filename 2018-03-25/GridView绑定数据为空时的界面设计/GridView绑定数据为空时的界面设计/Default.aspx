a5f8ba206c2684f4f872c8fc17e4da3aa3bf6e81
Clark-Chen-Dev <30585682+Clark-Chen-Dev@users.noreply.github.com>
2018-04-30 10:21:23 +0800

    GridView绑定数据为空时的界面设计

---
+++ 2018-03-25/GridView绑定数据为空时的界面设计/GridView绑定数据为空时的界面设计/Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridView绑定数据为空时的界面设计.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-size: 13px;">
+            <asp:GridView ID="GridViewEmptyDataShow" runat="server" AutoGenerateColumns="False"
+                EmptyDataText="无相关记录信息！" BackColor="White" BorderColor="LightGray"
+                BorderStyle="Double" BorderWidth="3px"
+                CellPadding="4" GridLines="Horizontal" Width="500px">
+
+                <Columns>
+                    <asp:BoundField DataField="name" HeaderText="编程词典版本">
+                        <ItemStyle BorderColor="LightGray" BorderStyle="Double" BorderWidth="1px"
+                            Width="100px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="location" HeaderText="内容简介">
+                        <ItemStyle BorderColor="LightGray" BorderStyle="Double" BorderWidth="1px" Width="300px" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="date" HeaderText="发布日期">
+                        <ItemStyle BorderColor="LightGray" BorderStyle="Double" BorderWidth="1px" Width="100px" />
+                    </asp:BoundField>
+                </Columns>
+
+                <FooterStyle BackColor="White" ForeColor="#333333" />
+                <RowStyle BackColor="White" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#339966" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#336666" ForeColor="White" HorizontalAlign="Center" />
+                <HeaderStyle BackColor="CornflowerBlue" Font-Bold="True" ForeColor="White" />
+            </asp:GridView>
+
+            <br />
+
+            <asp:Button ID="ButtonHasDataBind" runat="server" Text="数据源为空" Width="109px"
+                OnClick="ButtonHasDataBind_Click" />
+            <asp:Button ID="ButtonQueryEmptyBind" runat="server" Text="查询结果为空绑定" Width="142px" OnClick="ButtonQueryEmptyBind_Click" />
+            <asp:Button ID="ButtonConstructTableBind" runat="server" Text="构造空的DataTable绑定" Width="164px" OnClick="ButtonConstructTableBind_Click" />
+            <asp:Button ID="ButtonNormalBind" runat="server" Text="普通空数据绑定" Width="127px" OnClick="ButtonNormalBind_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2018-03-25/GridView绑定数据为空时的界面设计/GridView绑定数据为空时的界面设计/Default.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridView绑定数据为空时的界面设计
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                GridViewEmptyData();
+            }
+        }
+
+        private void GridViewEmptyData()
+        {
+            DataTable tbl = new DataTable(); //创建一个DataTable对象
+
+            //新增自定义的4列（即创建DataTable对象的骨架）
+            tbl.Columns.Add("id");
+            tbl.Columns.Add("name");
+            tbl.Columns.Add("location");
+            tbl.Columns.Add("date");
+
+            if (tbl.Rows.Count == 0)
+            {
+                tbl.Rows.Add(tbl.NewRow());
+            } // end if
+
+            GridViewEmptyDataShow.DataSource = tbl;
+            GridViewEmptyDataShow.DataBind();
+
+            //如果绑定的数据为空，则创建GridView表头并显示提示信息
+            int columnCount = tbl.Columns.Count;
+            GridViewEmptyDataShow.Rows[0].Cells.Clear();
+            GridViewEmptyDataShow.Rows[0].Cells.Add(new TableCell());
+            GridViewEmptyDataShow.Rows[0].Cells[0].ColumnSpan = columnCount;
+            GridViewEmptyDataShow.Rows[0].Cells[0].Text = "无相关记录信息！";
+            GridViewEmptyDataShow.Rows[0].Cells[0].Style.Add("text-align", "center");
+        }
+
+        /// <summary>
+        /// 数据源为空
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonHasDataBind_Click(object sender, EventArgs e)
+        {
+            if (GridViewEmptyDataShow.Rows.Count == 1 && GridViewEmptyDataShow.Rows[0].Cells[0].Text == "无相关记录信息！")
+            {
+                int columnCount = GridViewEmptyDataShow.Columns.Count;          //计算列数
+                GridViewEmptyDataShow.Rows[0].Cells.Clear();                    //删除没数据时的提示
+                GridViewEmptyDataShow.Rows[0].Cells.Add(new TableCell());       //创建新列的单无格
+                GridViewEmptyDataShow.Rows[0].Cells[0].ColumnSpan = columnCount;//计算列数
+                GridViewEmptyDataShow.Rows[0].Cells[0].Text = "无相关记录信息！";//数据为空时显示的提示信息
+                GridViewEmptyDataShow.Rows[0].Cells[0].Style.Add("text-align", "center");
+            }
+        }
+
+        /// <summary>
+        /// 查询结果为空绑定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonQueryEmptyBind_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            int columnCount = dt.Columns.Count;
+            GridViewEmptyDataShow.Rows[0].Cells.Clear();
+            GridViewEmptyDataShow.Rows[0].Cells.Add(new TableCell());
+            GridViewEmptyDataShow.Rows[0].Cells[0].ColumnSpan = columnCount;
+            GridViewEmptyDataShow.Rows[0].Cells[0].Text = "无相关记录信息！";
+            GridViewEmptyDataShow.Rows[0].Cells[0].Style.Add("text-align", "center");
+        }
+
+        /// <summary>
+        /// 构造空的DataTable绑定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonConstructTableBind_Click(object sender, EventArgs e)
+        {
+            DataTable tbl = new DataTable();
+            tbl.Columns.Add("id");
+            tbl.Columns.Add("name");
+            tbl.Columns.Add("location");
+            tbl.Columns.Add("date");
+            if (tbl.Rows.Count == 0)
+            {
+                tbl.Rows.Add(tbl.NewRow());
+            }
+            GridViewEmptyDataShow.DataSource = tbl;
+            GridViewEmptyDataShow.DataBind();
+        }
+
+        /// <summary>
+        /// 普通空数据绑定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonNormalBind_Click(object sender, EventArgs e)
+        {
+            DataTable tbl = new DataTable();
+            tbl.Columns.Add("id");
+            tbl.Columns.Add("name");
+            tbl.Columns.Add("location");
+            tbl.Columns.Add("date");
+            GridViewEmptyDataShow.DataSource = tbl;
+            GridViewEmptyDataShow.DataBind();
+        }
+    }
+}
