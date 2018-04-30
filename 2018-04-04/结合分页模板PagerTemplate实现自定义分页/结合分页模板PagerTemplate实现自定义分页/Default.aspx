56a13fdf399d399cf266f701a298573f04d9306a
Clark-Chen-Dev <30585682+Clark-Chen-Dev@users.noreply.github.com>
2018-04-30 18:01:22 +0800

    结合分页模板PagerTemplate实现自定义分页

---
+++ 2018-04-04/结合分页模板PagerTemplate实现自定义分页/结合分页模板PagerTemplate实现自定义分页/Default.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="结合分页模板PagerTemplate实现自定义分页.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>结合分页模板PagerTemplate实现自定义分页</title>
+    <style type="text/css">
+        body {
+            font-size: 9pt;
+        }
+
+        #main {
+            background-color: #EDF7E7;
+            line-height: 30px;
+            width: 100%;
+            text-align: left;
+        }
+
+        #info {
+            width: 40%;
+            font-family: Verdana;
+            float: left;
+            text-align: left;
+        }
+
+        #page {
+            width: 60%;
+            float: left;
+            overflow: auto;
+            text-align: right;
+        }
+
+        .txtInfo {
+            color: #e78a29;
+        }
+
+        .link {
+            text-decoration: none;
+            color: #474747;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"
+                AllowPaging="True" PageSize="5"
+                OnDataBound="GridView1_DataBound" CellPadding="2" CellSpacing="0"
+                Width="771px">
+                <PagerTemplate>
+                    <div id="main">
+                        <div id="info">
+                            &nbsp;&nbsp;页次：<asp:Label ID="lblPageCurrent" runat="server" Text="1" CssClass="txtInfo"></asp:Label>
+                            /<asp:Label ID="lblPageCount" runat="server" Text="1"></asp:Label>&nbsp;&nbsp;
+                  共&nbsp;<asp:Label ID="lblPageRow" runat="server" Text="1" CssClass="txtInfo"></asp:Label>&nbsp;条记录
+                        </div>
+                        <div id="page">
+                            <asp:LinkButton ID="btnFirst" runat="server" CssClass="link" CommandName="Pager" CommandArgument="First" OnCommand="NavigateToPage">[首页]</asp:LinkButton>&nbsp;
+                      <asp:LinkButton ID="btnPrev" runat="server" CssClass="link" CommandName="Pager" CommandArgument="Prev" OnCommand="NavigateToPage">[前一页]</asp:LinkButton>&nbsp;
+                      <asp:LinkButton ID="btnNext" runat="server" CssClass="link" CommandName="Pager" CommandArgument="Next" OnCommand="NavigateToPage">[下一页]</asp:LinkButton>&nbsp;
+                      <asp:LinkButton ID="btnLast" runat="server" CssClass="link" CommandName="Pager" CommandArgument="Last" OnCommand="NavigateToPage">[尾页]</asp:LinkButton>&nbsp;&nbsp;
+                        </div>
+                    </div>
+                </PagerTemplate>
+                <RowStyle BackColor="#F9F9F9" />
+                <HeaderStyle BackColor="#EDF7E7" Font-Bold="true" />
+                <AlternatingRowStyle BackColor="White" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2018-04-04/结合分页模板PagerTemplate实现自定义分页/结合分页模板PagerTemplate实现自定义分页/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 结合分页模板PagerTemplate实现自定义分页
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        int rowCount = 0;//用来记录总记录数
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                GetData();
+        }
+
+        private void GetData()
+        {
+            DataTable tbl = DBHelper.GetData("SELECT * FROM tb_OrderForm");
+            rowCount = tbl.Rows.Count;
+            GridView1.DataSource = tbl;
+            GridView1.DataBind();
+        }
+
+        protected void NavigateToPage(object sender, CommandEventArgs e)
+        {
+            switch (e.CommandArgument.ToString())//控制转页
+            {
+                case "First"://首页
+                    this.GridView1.PageIndex = 0;
+                    break;
+                case "Prev"://上一页
+                    if (this.GridView1.PageIndex > 0)
+                        this.GridView1.PageIndex -= 1;
+                    break;
+                case "Next"://下一页
+                    if (this.GridView1.PageIndex < (this.GridView1.PageCount - 1))
+                        this.GridView1.PageIndex += 1;
+                    break;
+                case "Last"://尾页
+                    this.GridView1.PageIndex = this.GridView1.PageCount - 1;
+                    break;
+            }
+            GetData();
+        }
+
+        protected void GridView1_DataBound(object sender, EventArgs e)
+        {
+            GridViewRow pagerRow = GridView1.BottomPagerRow;
+            //获取Label实例，显示页次信息
+            Label lblCurrent = (Label)pagerRow.Cells[0].FindControl("lblPageCurrent");
+            Label lblCount = (Label)pagerRow.Cells[0].FindControl("lblPageCount");
+            Label lblRow = (Label)pagerRow.Cells[0].FindControl("lblPageRow");
+            //获取按钮实例，为了控制其是否可用
+            LinkButton btnFirstTem = (LinkButton)pagerRow.Cells[0].FindControl("btnFirst");
+            LinkButton btnPrevTem = (LinkButton)pagerRow.Cells[0].FindControl("btnPrev");
+            LinkButton btnNextTem = (LinkButton)pagerRow.Cells[0].FindControl("btnNext");
+            LinkButton btnLastTem = (LinkButton)pagerRow.Cells[0].FindControl("btnLast");
+            if (lblCurrent != null)
+                lblCurrent.Text = (this.GridView1.PageIndex + 1).ToString();
+            if (lblCount != null)
+                lblCount.Text = this.GridView1.PageCount.ToString();
+            if (lblRow != null)
+                lblRow.Text = rowCount.ToString();
+            if (this.GridView1.PageIndex == 0)
+            {
+                btnFirstTem.Enabled = false;
+                btnPrevTem.Enabled = false;
+                //只有一页，所有分页按钮不可用
+                if (this.GridView1.PageCount == 1)
+                {
+                    btnNextTem.Enabled = false;
+                    btnLastTem.Enabled = false;
+                }
+            }
+            else if (this.GridView1.PageIndex == (this.GridView1.PageCount - 1))
+            {
+                btnNextTem.Enabled = false;
+                btnLastTem.Enabled = false;
+            }
+        }
+    }
+}
