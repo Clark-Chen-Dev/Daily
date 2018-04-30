1dd8550eada5c6eabf72328cb773ffc9310301c2
Clark-Chen-Dev <30585682+Clark-Chen-Dev@users.noreply.github.com>
2018-04-30 20:28:30 +0800

    在GridView控件中动态添加模板列

---
+++ 2018-04-05/在GridView控件中动态添加模板列/在GridView控件中动态添加模板列/Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="在GridView控件中动态添加模板列.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>GridView控件中动态添加模板列</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div align="center">
+            <table border="1" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="font-weight: bold">GridView控件中动态添加模板列</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" Font-Size="10.5pt" ForeColor="#333333">
+                            <Columns>
+                                <asp:BoundField HeaderText="标题" DataField="text" />
+                            </Columns>
+                            <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                            <RowStyle BackColor="#E3EAEB" />
+                            <EditRowStyle BackColor="#7C6F57" />
+                            <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                            <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                            <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                            <AlternatingRowStyle BackColor="White" />
+                        </asp:GridView>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="font-size: 9pt; height: 26px;">&nbsp;请选择要添加的模板列数：<asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem>1</asp:ListItem>
+                        <asp:ListItem>2</asp:ListItem>
+                        <asp:ListItem>3</asp:ListItem>
+                        <asp:ListItem>4</asp:ListItem>
+                        <asp:ListItem>5</asp:ListItem>
+                        <asp:ListItem>6</asp:ListItem>
+                        <asp:ListItem>7</asp:ListItem>
+                        <asp:ListItem>8</asp:ListItem>
+                        <asp:ListItem>9</asp:ListItem>
+                        <asp:ListItem>10</asp:ListItem>
+                        <asp:ListItem>15</asp:ListItem>
+                        <asp:ListItem>20</asp:ListItem>
+                    </asp:DropDownList>
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="添加模板列" Font-Size="10pt" /></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2018-04-05/在GridView控件中动态添加模板列/在GridView控件中动态添加模板列/Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 在GridView控件中动态添加模板列
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class GridViewTemplate : ITemplate
+        {
+            private DataControlRowType dcrType;
+            private string columnName;
+
+            public GridViewTemplate(DataControlRowType type, string colname)
+            {
+                dcrType = type;
+                columnName = colname;
+            }
+
+            public void InstantiateIn(Control container)
+            {
+                switch (dcrType)
+                {
+                    case DataControlRowType.Header:
+                        Literal literal = new Literal();
+                        literal.Text = columnName;
+                        container.Controls.Add(literal);
+                        break;
+                    case DataControlRowType.DataRow:
+                        DropDownList ddl = new DropDownList();
+                        ddl.ID = "dropdownlist";
+                        ddl.AppendDataBoundItems = true;
+                        ddl.Items.Add(new ListItem("-----请选择------", ""));
+                        ddl.Items.Add(new ListItem("ASP.NET全能编程词典", "98"));
+                        ddl.Items.Add(new ListItem("ASP.NET编程词典标准版", "368"));
+                        ddl.Items.Add(new ListItem("ASP.NET编程词典珍藏版", "698"));
+                        container.Controls.Add(ddl);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        ICollection CreateDataSource()
+        {
+            DataTable dt = new DataTable();//创建一个DataTable对象
+            DataRow dr;
+            //定义框架
+            dt.Columns.Add(new DataColumn("id", typeof(Int32)));
+            dt.Columns.Add(new DataColumn("text", typeof(string)));
+            for (int i = 0; i < 6; i++)//循环遍历并添充DataTable内容
+            {
+                dr = dt.NewRow();
+                dr[0] = i;
+                dr[1] = "列表" + i.ToString();
+                dt.Rows.Add(dr);
+            }
+            DataView dv = new DataView(dt);
+            return dv;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            for (int index = 1; index < GridView1.Columns.Count; index++)
+            {
+                GridView1.Columns[index].Visible = false;
+            }
+
+            for (int i = 0; i < Convert.ToInt32(DropDownList1.SelectedValue); i++)
+            {
+                TemplateField tf = new TemplateField();
+                tf.ShowHeader = true;
+                tf.HeaderTemplate = new GridViewTemplate(DataControlRowType.Header, "动态添加列");
+                tf.ItemTemplate = new GridViewTemplate(DataControlRowType.DataRow, "");
+                GridView1.Columns.Add(tf);
+                GridView1.DataSource = CreateDataSource();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
