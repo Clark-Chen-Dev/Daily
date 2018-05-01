116116f1c45d94bdb8a673dbaeadf33e554be646
Clark-Chen-Dev <30585682+Clark-Chen-Dev@users.noreply.github.com>
2018-05-01 14:39:05 +0800

    GridView删除数据时弹出确认对话框（含内容提示）

---
+++ 2018-04-09/GridView删除数据时弹出确认对话框（含内容提示）/GridView删除数据时弹出确认对话框（含内容提示）/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridView删除数据时弹出确认对话框_含内容提示_.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>供求信息网审核发布信息</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+            <asp:Panel ID="Panel2" runat="server">
+                <table align="center">
+                    <tr>
+                        <td style="font-size: 9pt; color: #ff0000; text-align: center">&nbsp;</td>
+                    </tr>
+                </table>
+                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+                    OnRowDataBound="GridView1_RowDataBound"
+                    OnSelectedIndexChanging="GridView1_SelectedIndexChanging" Font-Size="9pt"
+                    AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging"
+                    CellPadding="4" ForeColor="#333333" GridLines="None"
+                    OnRowDeleting="GridView1_RowDeleting">
+                    <Columns>
+                        <asp:BoundField DataField="id" HeaderText="信息ID" />
+                        <asp:BoundField DataField="name" HeaderText="信息主题" />
+                        <asp:BoundField DataField="type" HeaderText="信息分类" />
+                        <asp:BoundField DataField="userName" HeaderText="发布人" />
+                        <asp:BoundField DataField="lineMan" HeaderText="联系人" />
+                        <asp:BoundField DataField="term" HeaderText="有效期" DataFormatString="{0:d}" />
+                        <asp:BoundField DataField="check" HeaderText="审核" />
+                        <asp:CommandField HeaderText="通过/取消" SelectText="通过/取消" ShowSelectButton="True" />
+                        <asp:CommandField HeaderText="删除" ShowDeleteButton="True" />
+                    </Columns>
+                    <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                    <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+                    <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+                    <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Right" />
+                    <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                    <AlternatingRowStyle BackColor="White" />
+                </asp:GridView>
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2018-04-09/GridView删除数据时弹出确认对话框（含内容提示）/GridView删除数据时弹出确认对话框（含内容提示）/Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridView删除数据时弹出确认对话框_含内容提示_
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetData();
+            }
+        }
+
+        private void GetData()
+        {
+            GridView1.DataSource = DBHelper.GetData("select * from tb_inf");
+            GridView1.DataKeyNames = new string[] { "id" };
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if (e.Row.Cells[6].Text == "False")
+                {
+
+                    e.Row.Cells[6].Text = "<font color=red>已通过</font>";
+                }
+                else
+                {
+                    e.Row.Cells[6].Text = "未通过";
+                }
+                if (e.Row.RowState == DataControlRowState.Normal || e.Row.RowState == DataControlRowState.Alternate)
+                {
+                    //内容提示为GridView第二行数据显示的内容，其索引值为1（注意：.NET中索引值都是从0开始）
+                    ((LinkButton)e.Row.Cells[8].Controls[0]).Attributes.Add("onclick", "javascript:return confirm('确认要删除信息主题:\"" + e.Row.Cells[1].Text + "\"吗？')");
+                }
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetData();
+        }
+
+        protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            string id = GridView1.DataKeys[e.NewSelectedIndex].Value.ToString();
+            string sqlText = "update tb_inf set [check] = ~[check] where id = " + id;
+            if (DBHelper.ExecuteNonQuery(sqlText) == 0)
+            {
+                Response.Write("<script>alert('数据更新失败');</script>");
+            }
+            GetData();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int infoId = (int)GridView1.DataKeys[e.RowIndex].Value;//获取要删除的数据行的索引
+            string sqlText = "Delete from tb_inf where id=" + infoId;
+            if (DBHelper.ExecuteNonQuery(sqlText) > 0)
+            {
+                Response.Write("<script>alert('删除成功！')</script>");
+            } // end if
+            else
+            {
+                Response.Write("<script>alert('删除失败！')</script>");
+            }
+        }
+    }
+}
