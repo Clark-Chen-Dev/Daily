bb1e4af77529fadeccc9a87f3b14dc8fdb750ce2
Clark-Chen-Dev <30585682+Clark-Chen-Dev@users.noreply.github.com>
2018-05-01 15:24:40 +0800

    使用“…”优化GridView控件中的超长字符显示

---
+++ 2018-04-12/使用“…”优化GridView控件中的超长字符显示/使用“…”优化GridView控件中的超长字符显示/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="使用___优化GridView控件中的超长字符显示.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>供求信息网审核发布信息</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="text-align: center"><asp:Panel ID="Panel2" runat="server">
+        <table align="center">
+            <tr>
+                <td style="font-size: 9pt; color: #ff0000; text-align: center">&nbsp;</td>
+            </tr>
+        </table>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowDataBound="GridView1_RowDataBound" OnSelectedIndexChanging="GridView1_SelectedIndexChanging" Font-Size="9pt" AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <Columns>
+                    <asp:BoundField DataField="id" HeaderText="信息ID" />
+                    <asp:BoundField DataField="name" HeaderText="信息主题" />
+                    <asp:BoundField DataField="type" HeaderText="信息分类" />
+                    <asp:BoundField DataField="content" HeaderText="发布内容" />
+                    <asp:BoundField DataField="userName" HeaderText="发布人" />
+                    <asp:BoundField DataField="lineMan" HeaderText="联系人" />
+                    <asp:BoundField DataField="issueDate" HeaderText="有效期" 
+                        DataFormatString="{0:d}" />
+                </Columns>
+                <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+                <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Right" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <AlternatingRowStyle BackColor="White" />
+            </asp:GridView>
+        </asp:Panel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 2018-04-12/使用“…”优化GridView控件中的超长字符显示/使用“…”优化GridView控件中的超长字符显示/Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 使用___优化GridView控件中的超长字符显示
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                this.GetData();
+            }
+        }
+
+        public void GetData()
+        {
+            this.GridView1.DataSource = DBHelper.GetData("select * from tb_inf ");
+            this.GridView1.DataKeyNames = new string[] { "id" };
+            this.GridView1.DataBind();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                string gIntro = e.Row.Cells[3].Text;
+                e.Row.Cells[3].Text = GetFirstString(gIntro, 12);
+            }
+        }
+        protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            string id = GridView1.DataKeys[e.NewSelectedIndex].Value.ToString();
+            string sqlText = "update tb_inf set [check] = ~[check] where id = " + id;
+            if (DBHelper.ExecuteNonQuery(sqlText) == 0)
+            {
+                Response.Write("<script>alert('数据更新失败');</script>");
+            }
+            GetData();
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            this.GridView1.PageIndex = e.NewPageIndex;
+            this.GetData();
+        }
+
+        /// <summary>
+        /// 截取字符串优化版
+        /// </summary>
+        /// <param name="stringToSub">所要截取的字符串</param>
+        /// <param name="length">截取字符串的长度</param>
+        /// <returns></returns>
+        private string GetFirstString(string stringToSub, int length)
+        {
+            #region
+            Regex regex = new Regex("[\u4e00-\u9fa5]+", RegexOptions.Compiled);
+            char[] stringChar = stringToSub.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            int nLength = 0;
+            bool isCut = false;
+            for (int i = 0; i < stringChar.Length; i++)
+            {
+                if (regex.IsMatch((stringChar[i]).ToString()))//regex.IsMatch指示正则表达式在输入字符串中是否找到匹配项
+                {
+                    sb.Append(stringChar[i]);//将信息追加到当前 StringBuilder 的结尾
+                    nLength += 2;
+                }
+                else
+                {
+                    sb.Append(stringChar[i]);
+                    nLength = nLength + 1;
+                }
+                if (nLength > length)//替换字符串
+                {
+                    isCut = true;
+                    break;
+                }
+            }
+            if (isCut)
+                return sb.ToString() + "...";
+            else
+                return sb.ToString();
+            #endregion
+        }
+    }
+}
